1e7ba3337a7934428be8365297c6f8f171a7769f
Philippe Faist <phfaist@gmail.com>
2021-12-05 22:18:53 +0100

    jscomponents production build

--- jscomponents/webpack.config.js
+++ jscomponents/webpack.config.js
@@ -1,56 +1,61 @@
 const path = require("path");
 const webpack = require("webpack");
 
+
+const common_module_config = {
+    rules: [
+        {
+            test: /(\~)$/,
+            loader: "ignore-loader"
+        },
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: "babel-loader",
+            options: {
+                presets: [
+                    [
+                        "@babel/preset-env",
+                        {
+                            targets: "> 5%"
+                        }
+                    ]
+                ],
+                plugins: [
+                    "@babel/plugin-proposal-export-default-from",
+                    "@babel/plugin-proposal-class-properties",
+                    "@babel/plugin-syntax-top-level-await"
+                ],
+                sourceMaps: true
+            }
+        },
+        {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+        },
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+                // Creates `style` nodes from JS strings
+                'style-loader',
+                // Translates CSS into CommonJS
+                'css-loader',
+                // Compiles Sass to CSS
+                'sass-loader'
+            ]
+        }
+    ]
+};
+
 const EczSearchJSComponent = {
     entry: {
         "eczsearch": "./src/eczsearch/index.js",
     },
     //mode: "development",
-    module: {
-        rules: [
-            {
-                test: /(\~)$/,
-                loader: "ignore-loader"
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: "babel-loader",
-                options: {
-                    presets: [
-                        [
-                            "@babel/preset-env",
-                            {
-                                targets: "> 5%"
-                            }
-                        ]
-                    ],
-                    plugins: [
-                        "@babel/plugin-proposal-export-default-from",
-                        "@babel/plugin-proposal-class-properties",
-                        "@babel/plugin-syntax-top-level-await"
-                    ],
-                    sourceMaps: true
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    'sass-loader',
-                ]
-            }
-        ]
+    module: common_module_config,
+    resolve: {
+        extensions: ["*", ".js", ".jsx"]
     },
-    resolve: { extensions: ["*", ".js", ".jsx"] },
     output: {
         path: path.resolve(__dirname, "dist/"),
         publicPath: "/dist/",
