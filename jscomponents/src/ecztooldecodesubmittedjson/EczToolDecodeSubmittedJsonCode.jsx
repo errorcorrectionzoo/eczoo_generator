d25105af77cc1da9b843bde7c0002daf618da61c
Philippe Faist <phfaist@gmail.com>
2021-12-06 23:27:39 +0100

    internal tool to decode submitted JSON files

--- jscomponents/src/ecztooldecodesubmittedjson/EczToolDecodeSubmittedJsonCode.jsx
+++ jscomponents/src/ecztooldecodesubmittedjson/EczToolDecodeSubmittedJsonCode.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import YAML from 'yaml';
 
 
+import "./EczToolDecodeSubmittedJsonCode_style.scss";
+
+
 export default class EczToolDecodeSubmittedJsonCode extends React.Component
 {
     render()
@@ -35,33 +38,66 @@ export default class EczToolDecodeSubmittedJsonCode extends React.Component
     }
     set_json_content(json)
     {
-        console.log("Got JSON = ", json);
+        console.log("Got JSON = ", {data: json});
 
         const d = JSON.parse( json );
 
-        const yamldoc = new YAML.Document(d);
+        const yamldoc = new YAML.Document( d );
+
+        yamldoc.commentBefore =
+             '######################################################\n'
+            +'# This is a code entry in the error correction zoo. ##\n'
+            +'#       https://github.com/errorcorrectionzoo       ##\n'
+            +'######################################################\n';
 
         YAML.visit(
             yamldoc,
             {
+                Pair(_, pair, path) {
+                    console.log('pair =', pair, '; path =', path);
+                    if (path.length <= 6) { // [document, map object] for root map
+                        pair.key.spaceBefore = true;
+                    } else {
+                        pair.key.spaceBefore = false;
+                    }
+                },
+                Seq(key, node, path) {
+                    console.log('seq; node =', node, '; path =', path);
+                    node.items.forEach( (child, index) => {
+                        if (index > 0) {
+                            child.spaceBefore = true;
+                        }
+                    } );
+                },
+                Map(key, node, path) {
+                    console.log('map; node =', node, '; path =', path);
+                    node.items.forEach( (child, index) => {
+                        if (index > 0) {
+                            child.spaceBefore = true;
+                        }
+                    } );
+                },
                 Scalar(key, node) {
                     console.log('string; key =', key, ' node =', node);
                     if (key == 'key') {
                         node.type = 'PLAIN';
                     } else if (typeof node.value === 'string') {
-                        if (node.value.indexOf('\n') == -1) {
+                        let value = node.value;
+                        if (value.indexOf('\n') == -1) {
                             node.type = 'QUOTE_SINGLE';
                         } else {
                             node.type = 'BLOCK_LITERAL';
                         }
                     }
-                }
+                },
             }
         );
 
-        const yamlcode = yamldoc.toString();
+        const yamlcode = yamldoc.toString({
+            lineWidth: 0, // inhibit folding of single-quoted strings for Victor
+        });
 
-        console.log("Got YaML = ", yamlcode);
+        console.log("Got YaML = ", {data: yamlcode});
 
         this.setState({ output_yaml_code: yamlcode });
     }
