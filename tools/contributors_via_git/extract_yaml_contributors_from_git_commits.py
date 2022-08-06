44c37b31dc88fe57e4f02e6dd88fdad4eba5e18f
Philippe Faist <phfaist@gmail.com>
2022-08-06 12:43:54 +0200

    working on contributors extractor script

--- tools/contributors_via_git/extract_yaml_contributors_from_git_commits.py
+++ tools/contributors_via_git/extract_yaml_contributors_from_git_commits.py
@@ -18,6 +18,12 @@ this_root_dir = os.path.join(os.path.dirname(__file__), '..', '..')
 
 eczoo_site_setup_yml = os.path.join(this_root_dir, 'eczoo_site_setup.yml')
 
+
+
+with open(os.path.join(os.path.dirname(__file__), 'manual_code_renames.yml'), encoding='utf-8') as f:
+    manual_code_renames = yaml.safe_load(f)
+
+
 def runmain():
 
     logger = logging.getLogger(__name__)
@@ -38,7 +44,15 @@ def runmain():
         citation_extras = os.path.join(_root_dir, eczoo_site_setup['dirs']['citation_extras'])
 
     # 
-    eft = ExtractFromTree(Dirs.data_dir)
+    #branch = None
+    branch = '97b6405a1573507dc1b025b55eca19e3d27e9242' # DEBUG 1
+    #branch = '09937ec' # DEBUG 2
+    #
+
+    eft = ExtractFromTree(Dirs.data_dir,
+                          branch=branch,
+                          manual_file_renames=manual_code_renames['file_renames'])
+
     eft.extract()
 
     code_contributors = eft.get_code_contributors()
@@ -46,59 +60,41 @@ def runmain():
     import pprint
     pprint.pprint(code_contributors)
 
-    print("File renames:")
-    print(eft.file_renames.dumps())
-    print()
-    print("Code ID renames:")
-    print(eft.code_id_renames.dumps())
+    # print("File renames:")
+    # print(eft.file_renames.dumps())
+    # print()
+    # print("Code ID renames:")
+    # print(eft.code_id_renames.dumps())
 
 
 
 # ------------------------------------------------------------------------------
 
 
-class RenamesTracker:
-    r"""
-    Keep track of how names or identifiers might have been renamed in a history.
 
-    Add a rename rule with `add_rename(ts, old, new)` to signify that at time
-    `ts`, the name `old` was nenamed to `new`.
-
-    To see how a name `name` at time `ts` would translate to the most recent
-    name, use `apply_renames(ts, name)`.
-    """
-    def __init__(self):
+class DiffWithRenames:
+    def __init__(self, commit, parent, child=None):
         super().__init__()
-        # sorted in reverse order so that we only have to append new rules as we
-        # go back in time
-        self.renames_ts_list = []
-        self.renames_dict = {}
+        self.commit = commit
+        self.parent = parent
+        self.renames = {
+            'files': {},
+            'code_ids': {},
+        }
+        self.child = child
 
-    def add_rename(self, ts, old, new):
-        if ts not in self.renames_dict:
-            self.renames_dict[ts] = {}
-            self.renames_ts_list.append(ts)
-            self.renames_ts_list.sort(reverse=True)
-            
-        self.renames_dict[ts][old] = new
+    def add_rename(self, what, old, new):
+        self.renames[what][old] = new
 
-    def apply_renames(self, ts, name):
-        for tsx in reversed(self.renames_ts_list): # go chronologically
-            if tsx <= ts:
-                continue
-            name = self.renames_dict[tsx].get(name, name)
+    def apply_renames(self, what, name):
+        child = self
+        while child is not None:
+            name = child.renames[what].get(name, name)
+            child = child.child
         return name
 
-
-    def dumps(self):
-        lines = []
-        for ts in self.renames_ts_list:
-            dt = datetime.datetime.fromtimestamp(ts, tz=datetime.timezone.utc)
-            lines.append(f"@ {dt}")
-            for old, new in self.renames_dict[ts].items():
-                lines.append(f"    {old:>60} → {new}")
-        return "\n".join(lines)
-
+    def __repr__(self):
+        return f"<DiffWithRenames {getattr(self.parent,'hexsha','None')[:7]} → {getattr(self.commit, 'hexsha', None)[:7]}>"
 
 
 _rx_code_id = re.compile(
@@ -111,31 +107,62 @@ Contributor = collections.namedtuple('Contributor', ('name', 'email'))
 
 
 class ExtractFromTree:
-    def __init__(self, repo_dir):
-        super().__init__()
-        self.repo_dir = repo_dir
-        
-        self.repo = git.Repo(repo_dir)
-
-        self.file_renames = RenamesTracker()
-        self.code_id_renames = RenamesTracker()
-
-        self.collected_contribution_events = []
-
-    def extract(self, head_commit=None):
+    def __init__(self, repo_dir, branch=None, *, manual_file_renames=None):
 
         logger = logging.getLogger(__name__)
 
-        if head_commit is None:
-            head_commit = self.repo.head
-            
-        iter_commits = self.repo.iter_commits(paths='codes') #, max_count=100) # DEBUG
+        super().__init__()
+        self.repo_dir = repo_dir
+        self.branch = branch
 
-        for commit in iter_commits:
-            self._process_commit(commit)
+        # manual_file_renames = [ {hashdiff: [PARENTHASH,COMMITHASH], old: <old-fname>, new: <new-fname>}, ...]
+        self.manual_file_renames = manual_file_renames
+
+        self.manual_file_renames_x = {}
+        for ri in (self.manual_file_renames or []):
+            oldhash, newhash = ri['hashdiff']
+            oldfn, newfn = ri['old'], ri['new']
+            self.manual_file_renames_x.setdefault( (oldhash, newhash), {} )[oldfn] = newfn
+
+        logger.debug(f"{self.manual_file_renames_x=}")
+
+        self.repo = git.Repo(repo_dir)
+
+        if self.branch is None:
+            self.start_commit = self.repo.head.commit
+        elif self.branch in self.repo.heads:
+            self.start_commit = self.repo.heads[self.branch].commit
+        else:
+            self.start_commit = self.repo.commit(self.branch)
+
+        self.collected_contribution_events = []
+
+        self.diffswithrenames = {}
 
 
-    def _process_commit(self, commit):
+    def extract(self):
+
+        logger = logging.getLogger(__name__)
+
+        start_commit = self.start_commit
+        
+        # iter over ALL commits, not only those with changes to 'codes', so that
+        # we can see the full tree of commit changes (for our renames/diffs)
+        iter_commits = self.repo.iter_commits(start_commit)
+        #iter_commits = self.repo.iter_commits(start_commit, max_count=500) # DEBUG
+
+        diffxs_child = {}
+
+        iter_commits = list(iter_commits)
+
+        for commit_j, commit in enumerate(iter_commits):
+
+            logger.info(f"Processing commit {commit_j}/{len(iter_commits)} -- {commit.hexsha}")
+
+            self._process_commit(commit, diffxs_child)
+
+
+    def _process_commit(self, commit, diffxs_child):
 
         logger = logging.getLogger(__name__)
 
@@ -152,42 +179,63 @@ class ExtractFromTree:
 
         # Let's explore what changes were introduced in this commit
 
-        store_file_delete_new_info = {
-            'new': [],
-            'deleted': [],
-        }
-
         for commit_parent in commit.parents:
+
+            store_file_delete_new_info = {
+                'new': [],
+                'deleted': [],
+            }
+
+            diffx = DiffWithRenames(commit=commit, parent=commit_parent,
+                                    child=diffxs_child.get(commit.hexsha,None))
+
+            logger.debug(f"Setting up {diffx!r}")
+
+            # by registering only the child the first time we encounter a
+            # parent, we should heuristically follow the "main" branch when
+            # splitting branch & merge paths
+            diffxs_child.setdefault(commit_parent.hexsha, diffx)
 
             for diff in commit_parent.diff(commit, paths='codes'):
 
                 d = self._process_diff(
                     diff, commit, commit_parent, contributor,
+                    diffx=diffx,
                     store_file_delete_new_info=store_file_delete_new_info
                 )
 
-        new_files_by_code_id = {
-            fni['code_id']: fni['path']
-            for fni in store_file_delete_new_info['new']
-        }
+            #logger.debug(f"finally {store_file_delete_new_info=}")
 
-        for finfo in store_file_delete_new_info['deleted']:
-            if finfo['code_id'] in new_files_by_code_id:
-                # we found the file rename thanks to its code_id
-                self.file_renames.add_rename(
-                    commit_timestamp,
-                    finfo['path'],
-                    new_files_by_code_id[finfo['code_id']]
-                )
-            else:
-                # we don't know what this file was renamed to, or if it was
-                # simply deleted from the zoo.
-                self.file_renames.add_rename(commit_timestamp, diff.a_path,
-                                             f'DELETED-{commit.hexsha}-{diff.a_path}')
+            new_files_by_code_id = {
+                fni['code_id']: fni['path']
+                for fni in store_file_delete_new_info['new']
+                if fni['code_id']
+            }
+
+            #logger.debug(f"also {new_files_by_code_id=}")
+
+            for finfo in store_file_delete_new_info['deleted']:
+                del_path, del_codeid = finfo['path'], finfo['code_id']
+                if del_codeid in new_files_by_code_id:
+                    # we found the file rename thanks to its code_id
+                    diffx.add_rename(
+                        'files',
+                        del_path,
+                        new_files_by_code_id[del_codeid]
+                    )
+                else:
+                    # we don't know what this file was renamed to, or if it was
+                    # simply deleted from the zoo.
+                    diffx.add_rename(
+                        'files',
+                        del_path, f'DELETED-{commit.hexsha}-{del_path}'
+                    )
+
+            #logger.debug(f" --> diffx' file renames are {diffx.renames['files']}")
 
 
     def _process_diff(self, diff, commit, commit_parent, contributor, *,
-                      store_file_delete_new_info):
+                      diffx, store_file_delete_new_info):
 
         logger = logging.getLogger(__name__)
         logger.debug(f"Got diff for {diff.b_path}")
@@ -198,6 +246,12 @@ class ExtractFromTree:
         b_code_id = None
 
         file_path = diff.b_path
+        if file_path is None:
+            file_path = diff.a_path # for a deleted file
+
+        if not file_path.endswith('.yml'):
+            logger.debug(f"Skipping diff for non-YAML file {file_path}")
+            return
 
         if diff.new_file:
             logger.debug(f"File {diff.b_path!r} was created")
@@ -211,7 +265,7 @@ class ExtractFromTree:
 
         elif diff.renamed_file:
             logger.debug(f"File {diff.a_path!r} was renamed to {diff.b_path!r}")
-            self.file_renames.add_rename(commit_timestamp, diff.a_path, diff.b_path)
+            diffx.add_rename('files', diff.a_path, diff.b_path)
             a_code_id = get_code_id_from_yml_data(commit_parent, diff.a_path)
             b_code_id = get_code_id_from_yml_data(commit, diff.b_path)
 
@@ -221,7 +275,29 @@ class ExtractFromTree:
             logger.debug(f"File {diff.a_path!r} was deleted")
             file_path = diff.a_path
             a_code_id = get_code_id_from_yml_data(commit_parent, diff.a_path)
-            b_code_id = None
+
+            oldhash, newhash = str(commit_parent.hexsha), str(commit.hexsha)
+
+            #logger.debug(f"Checking for manual rename; {diff.a_path=},  {(oldhash, newhash)=}  {self.manual_file_renames_x=}")
+
+            if diff.a_path in self.manual_file_renames_x.get( (oldhash, newhash), {} ):
+                # rename was specified manually.
+                b_path = self.manual_file_renames_x[ (oldhash, newhash) ][ diff.a_path ]
+                b_code_id = get_code_id_from_yml_data(commit, b_path)
+
+                diffx.add_rename('files', diff.a_path, b_path)
+                if a_code_id != b_code_id:
+                    diffx.add_rename('code_ids', a_code_id, b_code_id)
+
+                logger.debug(f"Manual rename!  {diff.a_path} -> {b_path}   ({a_code_id} -> {b_code_id})")
+
+            else:
+                b_code_id = None
+
+                store_file_delete_new_info['deleted'].append(
+                    dict(path=diff.a_path, code_id=a_code_id)
+                )
+                #logger.debug(f"registered deleted file: {store_file_delete_new_info=}")
 
         else:
             assert diff.a_path == diff.b_path
@@ -231,19 +307,11 @@ class ExtractFromTree:
 
         if a_code_id is not None and b_code_id is not None and a_code_id != b_code_id:
             # there was a rename in the code ID as well.
-            self.code_id_renames.add_rename(commit_timestamp, a_code_id, b_code_id)
-
-        if not file_path.endswith('.yml'):
-            logger.debug(f"Skipping diff for non-YAML file {file_path}")
-            return
-
-        #file_path = self.file_renames.apply_renames(commit_timestamp, file_path)
+            diffx.add_rename('code_ids', a_code_id, b_code_id)
 
         code_id = b_code_id
         if code_id is None:
             code_id = a_code_id
-
-        #code_id = self.code_id_renames.apply_renames(commit_timestamp, code_id)
 
         # get precise word diff etc.  has_significant_diff will check all commit
         # parents so should be able to handle merges correctly.
@@ -251,7 +319,7 @@ class ExtractFromTree:
         if has_significant_diff(self.repo, commit, diff.a_path, diff.b_path):
 
             self.collected_contribution_events.append(
-                (commit_timestamp, code_id, file_path, contributor)
+                (commit, diffx, code_id, file_path, contributor)
             )
 
 
@@ -259,12 +327,43 @@ class ExtractFromTree:
         # process all code_id's and file_path's to apply any renames, and reduce
         # the contributors list.
 
+        logger = logging.getLogger(__name__)
+
+        start_commit = self.start_commit
+        logger.debug(f"start_commit's commit is {start_commit}")
+
         code_contributors = {}
 
-        for ts, code_id, file_path, contributor in reversed(self.collected_contribution_events):
+        for commit, diffx, code_id, file_path, contributor in \
+            reversed(self.collected_contribution_events):
+            #
             
-            code_id = self.code_id_renames.apply_renames(ts, code_id)
-            file_path = self.file_renames.apply_renames(ts, file_path)
+            orig_file_path = file_path
+
+            code_id = diffx.apply_renames('code_ids', code_id)
+            file_path = diffx.apply_renames('files', file_path)
+
+            try:
+                blob = start_commit.tree[file_path]
+            except KeyError:
+                logger.warning(
+                    f"Code file {file_path} no longer exists and could not be traced.  A "
+                    f"contribution in commit {commit} was authored by {contributor} on {orig_file_path}."
+                )
+                # DEBUG:
+                logger.debug("Attempted to trace renames:")
+                dx = diffx
+                fn = orig_file_path
+                while dx is not None:
+                    logger.debug(f"  @ {dx.commit.hexsha[:7]}")
+                    if fn in dx.renames['files']:
+                        logger.debug(f"  @ {dx.commit.hexsha[:7]}:  {fn} → {dx.renames['files'][fn]}")
+                        fn = dx.renames['files'][fn]
+                        if fn.startswith('DELETED-'):
+                            logger.debug(f"      DELETED [{dx.parent.hexsha}, {dx.commit.hexsha}]")
+                            break
+                    dx = dx.child
+                continue
 
             key = (code_id, file_path)
 
@@ -323,12 +422,12 @@ def compute_diff_score(thediff, file_path_old, file_path_new):
     
     logger = logging.getLogger(__name__)
 
-    logger.debug("the diff is:\n"+thediff)
+    #logger.debug("the diff is:\n"+thediff)
     difflines = thediff.strip().split('\n')
 
-    logger.debug(f"{file_path_old=} {file_path_new=}")
+    # logger.debug(f"{file_path_old=} {file_path_new=}")
 
-    logger.debug(f"{difflines=}")
+    # logger.debug(f"{difflines=}")
 
     if not difflines or not difflines[0]:
         return 0
@@ -393,17 +492,18 @@ def has_significant_diff(repo, commit, file_path_old, file_path_new):
     diffscore = min(scores)
 
     if diffscore > diff_score_threshold:
-        logger.debug(f"Changes by {commit.author.name} to {file_path_new} on {commit} "
-                     f"are indeed significant! ({diffscore=})")
+        # logger.debug(f"Changes by {commit.author.name} to {file_path_new} on {commit} "
+        #              f"are indeed significant! ({diffscore=})")
         return True
 
-    logger.debug(f"Changes by {commit.author.name} to {file_path_new} on {commit} "
-                 f"are not ruled as significant ({diffscore=})")
+    # logger.debug(f"Changes by {commit.author.name} to {file_path_new} on {commit} "
+    #              f"are not ruled as significant ({diffscore=})")
     return False
 
 
 # ------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger('git.cmd').setLevel(logging.INFO)
     runmain()
